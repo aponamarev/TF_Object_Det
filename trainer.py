f559835f1a9c0e1ed7f7ff1f7d2367438c5fb292
Alex Ponamarev <alex.ponamaryov@gmail.com>
2017-02-05 17:53:41 -0500

    Trainer is updated to the minimal viable stage with the total training procedure functioning properly. Tensor Board is live as well.
    Changes to be committed:
    	modified:   trainer.py

--- trainer.py
+++ trainer.py
@@ -1,6 +1,8 @@
 # enable import using multiline statements (useful when import many items in one statement)
 from __future__ import absolute_import
+import os
 import tensorflow as tf
+import numpy as np
 # import config for kitti
 from src.config import kitti_squeezeDet_config
 # import dataset class for kitti set
@@ -47,17 +49,19 @@ def train():
             1. Check that the provided dataset can be processed
             2. Setup a config and the model for squeezeDet
             3. Initialize a image database
-            4. Initialize variables in the model
+            4. Initialize variables in the model and merge all summaries
             5. Read a minibatch of data
             6. Convert a 2d arrays of inconsistent size (varies based on n of objests) into a list of tuples or tripples
+            7. Configure operation that TF should run depending on the step number
+            8. Save the model checkpoint periodically.
     """
     #1. Check that the provided dataset can be processed
     assert FLAGS.dataset in ['KITTI'], \
         'Currently only support KITTI dataset'
     assert FLAGS.net in ['vgg16','resnet50', 'squeezeDet','squeezeDet+'], \
         'Selected neural net architecture not supported: {}'.format(FLAGS.net)
-
-    with tf.Graph().as_default():
+    graph = tf.Graph()
+    with graph.as_default():
         #2. Setup a config and the model for squeezeDet
         if FLAGS.net == 'squeezeDet':
             mc = kitti_squeezeDet_config()
@@ -67,9 +71,13 @@ def train():
         #3. Initilize a image database
         imdb = kitti(data_path=FLAGS.data_path, image_set=FLAGS.image_set, mc=mc)
 
-    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)):
-        #4. Initialize variables in the model
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True), graph=graph) as sess:
+        #4. Initialize variables in the model and merge all summaries
         tf.initialize_all_variables().run()
+        saver = tf.train.Saver(tf.all_variables())
+        summary_op = tf.merge_all_summaries()
+
+        tf.train.start_queue_runners(sess=sess)
 
         for step in xrange(FLAGS.max_steps):
             #5. Read a minibatch of data
@@ -78,27 +86,85 @@ def train():
 
             # 6. Convert a 2d arrays of inconsistent size (varies based
             # on n of objests) into a list of tuples or tripples
-            label_indices, bbox_indices, box_delta_values, mask_indices,\
-            box_values = convertToFixedSize(aidx_per_batch=aidx_per_batch,
-                                            label_per_batch=label_per_batch,
-                                            box_delta_per_batch=box_delta_per_batch,
-                                            bbox_per_batch=bbox_per_batch)
-
-            # mask_indices is a list of all anchors responsible for object detection
-            # (tuple [image id, anchor id] for each object)
-
-
+            label_indices, \
+            bbox_indices, \
+            box_delta_values, \
+            mask_indices, \
+            box_values = convertToFixedSize(
+                aidx_per_batch=aidx_per_batch,
+                label_per_batch=label_per_batch,
+                box_delta_per_batch=box_delta_per_batch,
+                bbox_per_batch=bbox_per_batch
+            )
 
             feed_dict = {
-                model.image_input: image_per_batch,
                 model.keep_prob: mc.KEEP_PROB,
-                model.input_mask: sparse_to_dense(mask_indices, [mc.BATCH_SIZE, mc.ANCHORS])
 
+                model.image_input: image_per_batch,
+
+                model.input_mask: np.reshape(
+                    sparse_to_dense(mask_indices,[mc.BATCH_SIZE, mc.ANCHORS],
+                                    np.ones(len(mask_indices), dtype=np.float)),
+                    [mc.BATCH_SIZE, mc.ANCHORS, 1]),
+
+                model.box_delta_input: sparse_to_dense(
+                    bbox_indices, [mc.BATCH_SIZE, mc.ANCHORS, 4],
+                    box_delta_values),
+
+                model.box_input: sparse_to_dense(
+                    bbox_indices, [mc.BATCH_SIZE, mc.ANCHORS, 4],
+                    box_values),
+
+                model.labels: sparse_to_dense(
+                    label_indices, [mc.BATCH_SIZE, mc.ANCHORS, mc.CLASSES],
+                    np.ones(len(label_indices), dtype=np.float))
             }
 
+            # 7. Configure operation that TF should run depending on the step number
+            if step % FLAGS.summary_step == 0:
+                op_list = [
+                    model.train_op,
 
+                    model.loss,
+                    summary_op,
+                    model.det_boxes,
+                    model.det_probs,
+                    model.det_class,
+                    model.conf_loss,
+                    model.bbox_loss,
+                    model.class_loss
+                ]
 
+                _, \
+                loss_value, \
+                summary_str, \
+                det_boxes, \
+                det_probs, \
+                det_class, \
+                conf_loss, \
+                bbox_loss, \
+                class_loss = sess.run(op_list, feed_dict=feed_dict)
 
+            else:
+                _, \
+                loss_value, \
+                conf_loss, \
+                bbox_loss, \
+                class_loss = sess.run(
+                    [model.train_op,
+                     model.loss,
+                     model.conf_loss,
+                     model.bbox_loss,
+                     model.class_loss], feed_dict=feed_dict)
+
+            assert not np.isnan(loss_value), \
+                'Model diverged. Total loss: {}, conf_loss: {}, bbox_loss: {}, ' \
+                'class_loss: {}'.format(loss_value, conf_loss, bbox_loss, class_loss)
+
+            #8. Save the model checkpoint periodically.
+            if step % FLAGS.checkpoint_step == 0 or (step + 1) == FLAGS.max_steps:
+                checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+                saver.save(sess, checkpoint_path, global_step=step)
 
 
 if __name__ == '__main__':
