dddeb64577c7b1a3a4a4c21db0c41551cbb36ed2
Alex Ponamarev <alex.ponamaryov@gmail.com>
2017-02-05 18:29:34 -0500

    Isolated a part responsible for graph creating into a separate function for clarity

--- trainer.py
+++ trainer.py
@@ -3,13 +3,6 @@ from __future__ import absolute_import
 import os
 import tensorflow as tf
 import numpy as np
-# import config for kitti
-from src.config import kitti_squeezeDet_config
-# import dataset class for kitti set
-from src.dataset import kitti
-# import a net that you'll use
-from src.nets import SqueezeDet
-# import visualization function
 from src.utils.visualization import vis_img
 # import conversion from sparse to dense arrays
 from src.utils.util import sparse_to_dense, convertToFixedSize
@@ -44,35 +37,63 @@ tf.app.flags.DEFINE_integer('checkpoint_step', 1000,
                         """Number of steps to save summary.""")
 tf.app.flags.DEFINE_string('gpu', '0', """gpu id.""")
 
-def train():
-    """ Executes training procedure that includes:
-            1. Check that the provided dataset can be processed
-            2. Setup a config and the model for squeezeDet
-            3. Initialize a image database
-            4. Initialize variables in the model and merge all summaries
-            5. Read a minibatch of data
-            6. Convert a 2d arrays of inconsistent size (varies based on n of objests) into a list of tuples or tripples
-            7. Configure operation that TF should run depending on the step number
-            8. Save the model checkpoint periodically.
+def defineComputGraph(FLAGS):
     """
-    #1. Check that the provided dataset can be processed
+    1. Check that the provided dataset can be processed
+    2. Setup a config and the model for squeezeDet
+    3. Initialize a image database
+
+    :param FLAGS: Should contain    FLAGS.pretrained_model_path,
+                                    FLAGS.data_path
+                                    FLAGS.image_set
+                                    FLAGS.pretrained_model_path
+                                    FLAGS.gpu
+
+    :return: model and the database
+    """
+    # import config for kitti
+    from src.config import kitti_squeezeDet_config
+    # import dataset class for kitti set
+    from src.dataset import kitti
+    # import a net that you'll use
+    from src.nets import SqueezeDet
+    # import visualization function
+
+    # 1. Check that the provided dataset can be processed
     assert FLAGS.dataset in ['KITTI'], \
         'Currently only support KITTI dataset'
-    assert FLAGS.net in ['vgg16','resnet50', 'squeezeDet','squeezeDet+'], \
+    assert FLAGS.net in ['vgg16', 'resnet50', 'squeezeDet', 'squeezeDet+'], \
         'Selected neural net architecture not supported: {}'.format(FLAGS.net)
+
     graph = tf.Graph()
     with graph.as_default():
-        #2. Setup a config and the model for squeezeDet
+
+        # 2. Setup a config and the model for squeezeDet
         if FLAGS.net == 'squeezeDet':
             mc = kitti_squeezeDet_config()
             mc.PRETRAINED_MODEL_PATH = FLAGS.pretrained_model_path
             model = SqueezeDet(mc, FLAGS.gpu)
 
-        #3. Initilize a image database
+        # 3. Initilize a image database
         imdb = kitti(data_path=FLAGS.data_path, image_set=FLAGS.image_set, mc=mc)
+        return graph, model, imdb, mc
+
+def train():
+    """ Executes training procedure that includes:
+            1. Create a computational graph, model, database, and a controller
+            2. Initialize variables in the model and merge all summaries
+            3. Read a minibatch of data
+            4. Convert a 2d arrays of inconsistent size (varies based on n of objests) into a list of tuples or tripples
+            5. Configure operation that TF should run depending on the step number
+            6. Save the model checkpoint periodically.
+    """
+
+    # 1. Create a computational graph, model, database, and a controller
+    graph, model, imdb, mc = defineComputGraph(FLAGS)
 
     with tf.Session(config=tf.ConfigProto(allow_soft_placement=True), graph=graph) as sess:
-        #4. Initialize variables in the model and merge all summaries
+
+        # 2. Initialize variables in the model and merge all summaries
         tf.initialize_all_variables().run()
         saver = tf.train.Saver(tf.all_variables())
         summary_op = tf.merge_all_summaries()
@@ -80,11 +101,11 @@ def train():
         tf.train.start_queue_runners(sess=sess)
 
         for step in xrange(FLAGS.max_steps):
-            #5. Read a minibatch of data
+            # 3. Read a minibatch of data
             image_per_batch, label_per_batch, box_delta_per_batch, \
             aidx_per_batch, bbox_per_batch = imdb.read_batch()
 
-            # 6. Convert a 2d arrays of inconsistent size (varies based
+            # 4. Convert a 2d arrays of inconsistent size (varies based
             # on n of objests) into a list of tuples or tripples
             label_indices, \
             bbox_indices, \
@@ -120,7 +141,7 @@ def train():
                     np.ones(len(label_indices), dtype=np.float))
             }
 
-            # 7. Configure operation that TF should run depending on the step number
+            # 5. Configure operation that TF should run depending on the step number
             if step % FLAGS.summary_step == 0:
                 op_list = [
                     model.train_op,
@@ -161,7 +182,7 @@ def train():
                 'Model diverged. Total loss: {}, conf_loss: {}, bbox_loss: {}, ' \
                 'class_loss: {}'.format(loss_value, conf_loss, bbox_loss, class_loss)
 
-            #8. Save the model checkpoint periodically.
+            # 6. Save the model checkpoint periodically.
             if step % FLAGS.checkpoint_step == 0 or (step + 1) == FLAGS.max_steps:
                 checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
                 saver.save(sess, checkpoint_path, global_step=step)
