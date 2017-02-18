0c914a0aad61343ae76342e864b7087ea0ca2970
Alex Ponamarev <alex.ponamaryov@gmail.com>
2017-02-18 00:48:06 -0500

    Simplified Resize class to allow multiple calls to dimension_targets

--- src/dataset/Resize.py
+++ src/dataset/Resize.py
@@ -2,7 +2,7 @@
 Resize class is a helper class designed to resize images and annotations for COCO.
 Implementation for others classes will be explored later. Current implementation includes the following methods:
     :method: imResize(img) -> image
-    :method: bboxResize([x,y,h,w]) - [x_r, y_r, h_r, w_r]
+    :method: bboxResize([x,y,w,h]) - [x_r, y_r, w_r, h_r]
 
 The class is initialized with:
     :param: dimensions - 1d [height, width] array
@@ -39,12 +39,7 @@ class Resize(object):
         """
         Ensures that dimesion_recieved read only once. The data will be deleted after it is read.
         """
-        # make sure that dimension_reveived is not None
-        assert not self.__dimension_received == None,\
-            "dimension_recieved has value None. The error caused by reading this variable more than once."
-        values = self.__dimension_received
-        self.__dimension_received = None
-        return values
+        return self.__dimension_received
 
     @dimension_received.setter
     def dimension_received(self, values):
@@ -74,10 +69,12 @@ class Resize(object):
         # 1. set received_dimensions
         shape = []
         try:
-            shape = im.shape[:2]
+            h, w, _ = [float(dim) for dim in im.shape]
+            shape = (w,h)
         except ArithmeticError:
             im = np.array(im)
-            shape = im.shape[:2]
+            h, w, _ = [float(dim) for dim in im.shape]
+            shape = (w, h)
         except:
             print "Unexpected error:", sys.exc_info()[0]
             raise
@@ -87,17 +84,46 @@ class Resize(object):
         im = cv2.resize(im, self.dimension_targets)
         return im
 
+    def bboxResize(self, gt_bbox):
+        """
+        Addjust the gt_bbox based on the new dimensions of the image (provided that image was resized first <- otherwise
+        error should be raised).
+
+        :param gt_bbox: 1d [x, y, width, height] array
+
+        :return: resized_gt_bbox
+        """
+        assert type(gt_bbox)==list, "gt_bbox has incorrect datatype. List is expected. {} was received."\
+            .format(type(gt_bbox).__name__)
+
+        received_dim = self.dimension_received
+        target_dim = self.dimension_targets
+        width_adj, height_adj = target_dim[0]/float(received_dim[0]), target_dim[1]/float(received_dim[1])
+        gt_bbox[0::2] = [int(gt_bbox[0]*width_adj), int(gt_bbox[2]*width_adj)]
+        gt_bbox[1::2] = [int(gt_bbox[1]*height_adj), int(gt_bbox[3]*height_adj)]
+        return gt_bbox
+
 
 if __name__ == '__main__':
-    from src.dataset import ImRead
+    import ImRead
     imread = ImRead()
     path = '/Users/aponamaryov/Downloads/coco_train_2014/images/COCO_train2014_000000000659.jpg'
 
     rsize = Resize(dimensinos=(1000, 500))
     im = imread.read(path)
-    plt.imshow(misc.toimage(im))
+    im_ann = im.copy()
+    ann = [275, 230, 340, 320]
+    im_ann = cv2.rectangle(im_ann,
+                               (ann[0]-ann[2]/2, ann[1]-ann[3]/2), (ann[0]+ann[2]/2, ann[1]+ann[3]/2),
+                               color=256, thickness=3)
+    plt.imshow(misc.toimage(im_ann))
     resized_im = rsize.imResize(im)
-    plt.imshow(misc.toimage(resized_im))
+    resized_ann = rsize.bboxResize(ann)
+    resized_im_ann = cv2.rectangle(resized_im,
+                                       (resized_ann[0] - resized_ann[2] / 2, resized_ann[1] - resized_ann[3] / 2),
+                                       (resized_ann[0] + resized_ann[2] / 2, resized_ann[1] + resized_ann[3] / 2),
+                                       color=256, thickness=2)
+    plt.imshow(misc.toimage(resized_im_ann))
     print "resize verification completed successfully!"
 
 
