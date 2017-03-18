0a20016b945844110714a9de97b65edf8e9ac17c
aponamarev <alex.ponamaryov@gmail.com>
2017-03-18 00:22:53 +0000

    Updated to work with GPU

--- trainer_COCO_tf_v1.py
+++ trainer_COCO_tf_v1.py
@@ -15,7 +15,7 @@ FLAGS = tf.app.flags.FLAGS
 # define data set flags
 tf.app.flags.DEFINE_string('dataset', 'COCO',
                        """This trainier addopted specifically for COCO dataset. Please use other trainers for other datasets.""")
-tf.app.flags.DEFINE_string('IMAGES_PATH', 'images',
+tf.app.flags.DEFINE_string('IMAGES_PATH', 'images/train2014',
                            """Provide a path to where the images are stored.""")
 tf.app.flags.DEFINE_string('ANNOTATIONS_FILE_NAME', 'annotations/instances_train2014.json',
                            """Please provide an absolute path to the annotations file.""")
@@ -25,7 +25,7 @@ tf.app.flags.DEFINE_string('ANNOTATIONS_FILE_NAME', 'annotations/instances_train
 tf.app.flags.DEFINE_string('net', 'squeezeDet',
                        """Neural net architecture. """)
 tf.app.flags.DEFINE_string('pretrained_model_path',
-                           'logs/squeezeDet1024x1024/train/model.ckpt-0',
+                           'logs/train/model.ckpt-0',
                            """Path to the pretrained model.""")
 
 # define training flags
@@ -46,7 +46,7 @@ MC.IMAGES_PATH = FLAGS.IMAGES_PATH
 MC.ANNOTATIONS_FILE_NAME = FLAGS.ANNOTATIONS_FILE_NAME
 MC.OUTPUT_RES = (24, 24)
 MC.RESIZE_DIM = (768, 768)
-MC.BATCH_SIZE = 10
+MC.BATCH_SIZE = 100
 MC.BATCH_CLASSES = ['person', 'car', 'bicycle']
 
 IMDB = COCO(coco_name='train',
@@ -70,12 +70,11 @@ def train():
     """
     graph = tf.Graph()
     with graph.as_default():
-        #with tf.device("/gpu:{}".format(FLAGS.gpu)):
         # 1. Create a dataset and a controller
         cqr = CQR(IMDB.get_sample, len(IMDB.ANCHOR_BOX), len(IMDB.CLASS_NAMES_AVAILABLE),
                   img_size=[MC.IMAGE_WIDTH, MC.IMAGE_HEIGHT,3],
                   batch_size=MC.BATCH_SIZE)
-
+        q_size = tf.summary.scalar("queue_size",cqr.q_size)
         input_dict = {}
         input_dict['image_input'],\
         input_dict['labels'],\
@@ -90,81 +89,84 @@ def train():
         summary_op = tf.summary.merge_all()
         summary_writer = tf.summary.FileWriter(FLAGS.train_dir, graph)
 
-    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True), graph=graph) as sess:
-        print("Beginning training process.")
-        sess.run(initializer)
         # Launch coordinator that will manage threads
         coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    graph.finalize()
 
-        pass_tracker_start = time.time()
-        pass_tracker_prior = pass_tracker_start
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True), graph=graph)
+    print("Beginning training process.")
+    sess.run(initializer)
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
-        cqr.fill_q(sess)
+    pass_tracker_start = time.time()
+    pass_tracker_prior = pass_tracker_start
 
-        prior_step = 0
+    cqr.fill_q(sess)
 
-        for step in range(FLAGS.max_steps):
+    prior_step = 0
 
-            # 5. Configure operation that TF should run depending on the step number
-            if step % FLAGS.summary_step == 0:
-                op_list = [
-                    model.train_op,
-                    model.loss,
-                    summary_op,
-                    model.det_boxes,
-                    model.det_probs,
-                    model.det_class,
-                    model.conf_loss,
-                    model.bbox_loss,
-                    model.class_loss
-                ]
+    for step in range(FLAGS.max_steps):
 
-                _, loss_value, summary_str, det_boxes, det_probs, det_class, conf_loss, bbox_loss, class_loss = \
-                    sess.run(op_list, feed_dict={model.keep_prob: MC.KEEP_PROB})
+        # 5. Configure operation that TF should run depending on the step number
+        if step % FLAGS.summary_step == 0:
+            op_list = [
+                model.train_op,
+                model.loss,
+                summary_op,
+                model.det_boxes,
+                model.det_probs,
+                model.det_class,
+                model.conf_loss,
+                model.bbox_loss,
+                model.class_loss
+            ]
 
-                pass_tracker_end = time.time()
+            _, loss_value, summary_str, det_boxes, det_probs, det_class, conf_loss, bbox_loss, class_loss = \
+                sess.run(op_list, feed_dict={model.keep_prob: MC.KEEP_PROB})
 
-                viz_summary = sess.run(model.viz_op, feed_dict={model.image_to_show: model.image_input.eval()})
-                summary_writer.add_summary(summary_str, step)
-                summary_writer.add_summary(viz_summary, step)
+            pass_tracker_end = time.time()
 
-                #Report results
-                number_of_steps = step - prior_step
-                number_of_steps = number_of_steps if number_of_steps > 0 else 1
-                print('\nStep: {}. Timer: {} network passes (with batch size {}): {:.1f} seconds ({:.1f} per batch). Losses: conf_loss: {:.3f}, bbox_loss: {:.3f}, class_loss: {:.3f} and total_loss: {:.3f}'.
-                       format(step, number_of_steps, IMDB.mc.BATCH_SIZE,
-                              pass_tracker_end - pass_tracker_prior, (pass_tracker_end - pass_tracker_prior)/number_of_steps,
-                              conf_loss, bbox_loss, class_loss, loss_value))
-                pass_tracker_prior = pass_tracker_end
-                prior_step = step
+            viz_summary = sess.run(model.viz_op, feed_dict={model.image_to_show: sess.run(model.image_input)})
+            summary_writer.add_summary(summary_str, step)
+            summary_writer.add_summary(viz_summary, step)
 
-            else:
-                _, loss_value, conf_loss, bbox_loss, class_loss = \
-                    sess.run([model.train_op, model.loss, model.conf_loss, model.bbox_loss, model.class_loss],
-                             feed_dict={model.keep_prob: MC.KEEP_PROB})
-                print(".", end="")
+            #Report results
+            number_of_steps = step - prior_step
+            number_of_steps = number_of_steps if number_of_steps > 0 else 1
+            print('\nStep: {}. Timer: {} network passes (with batch size {}): {:.1f} seconds ({:.1f} per batch). Losses: conf_loss: {:.3f}, bbox_loss: {:.3f}, class_loss: {:.3f} and total_loss: {:.3f}'.
+                   format(step, number_of_steps, IMDB.mc.BATCH_SIZE,
+                          pass_tracker_end - pass_tracker_prior, (pass_tracker_end - pass_tracker_prior)/number_of_steps,
+                          conf_loss, bbox_loss, class_loss, loss_value))
+            pass_tracker_prior = pass_tracker_end
+            prior_step = step
 
-            assert not np.isnan(loss_value), \
-                'Model diverged. Total loss: {}, conf_loss: {}, bbox_loss: {}, ' \
-                'class_loss: {}'.format(loss_value, conf_loss, bbox_loss, class_loss)
+        else:
+            _, loss_value, conf_loss, bbox_loss, class_loss = \
+                sess.run([model.train_op, model.loss, model.conf_loss, model.bbox_loss, model.class_loss],
+                         feed_dict={model.keep_prob: MC.KEEP_PROB})
+            print(".", end="")
 
-            # 6. Save the model checkpoint periodically.
-            if step % FLAGS.checkpoint_step == 0 or (step + 1) == FLAGS.max_steps:
-                viz_summary = sess.run(model.viz_op, feed_dict={model.image_to_show: model.image_input.eval()})
-                summary_writer.add_summary(summary_str, step)
-                summary_writer.add_summary(viz_summary, step)
-                checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
-                saver.save(sess, checkpoint_path, global_step=step)
+        assert not np.isnan(loss_value), \
+            'Model diverged. Total loss: {}, conf_loss: {}, bbox_loss: {}, ' \
+            'class_loss: {}'.format(loss_value, conf_loss, bbox_loss, class_loss)
 
-            # Prefetch data
-            cqr.fill_q_async(sess)
+        # 6. Save the model checkpoint periodically.
+        if step % FLAGS.checkpoint_step == 0 or (step + 1) == FLAGS.max_steps:
+            viz_summary = sess.run(model.viz_op, feed_dict={model.image_to_show: sess.run(model.image_input)})
+            summary_writer.add_summary(summary_str, step)
+            summary_writer.add_summary(viz_summary, step)
+            checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+            saver.save(sess, checkpoint_path, global_step=step)
 
-        # Close a queue and cancel all elements in the queue. Request coordinator to stop all the threads.
-        sess.run(CQR.queue.close(cancel_pending_enqueues=True))
-        coord.request_stop()
-        # Tell coordinator to stop any queries to the threads
-        coord.join(threads)
+        # Prefetch data
+        cqr.fill_q_async(sess)
+
+    # Close a queue and cancel all elements in the queue. Request coordinator to stop all the threads.
+    sess.run(CQR.queue.close(cancel_pending_enqueues=True))
+    coord.request_stop()
+    # Tell coordinator to stop any queries to the threads
+    coord.join(threads)
+    sess.close()
 
 
 if __name__ == '__main__':
